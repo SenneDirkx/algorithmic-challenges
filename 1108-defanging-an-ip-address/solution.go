c4e63d4c5fa4f1a51f8a19be1fa43542397f30db
Senne Dirkx <dirkxsenneinc@gmail.com>
2019-09-06 18:16:22 +0200

    fixed go syntax

--- 1108-defanging-an-ip-address/solution.go
+++ 1108-defanging-an-ip-address/solution.go
@@ -1,13 +1,19 @@
+package main
+
+func main() {
+	defangIPaddr("1.1.1.1")
+}
+
 func defangIPaddr(address string) string {
-    new_address := ""
-    for i := 0; i < len(address); i++ {
-        if string(address[i]) == "." {
-            new_address = new_address + "[.]"
-        } else {
-            new_address = new_address + string(address[i])
-        }
-    }
-    return new_address
+	newAddress := ""
+	for i := 0; i < len(address); i++ {
+		if string(address[i]) == "." {
+			newAddress = newAddress + "[.]"
+		} else {
+			newAddress = newAddress + string(address[i])
+		}
+	}
+	return newAddress
 }
 
 // Runtime: 0 ms (faster than 100% of online Go submissions)
